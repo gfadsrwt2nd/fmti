65aa8dc99238cbb0d0f6fc2f25d4735a13e07be8
gmzr0x <55633963+gmzr0x@users.noreply.github.com>
2020-11-09 02:12:19 +0800

    Add files via upload

---
+++ anggota/pengawas/heryanti.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+def heryanti():
+	time.sleep(0.8)
+	os.system('clear')
+	print("    __  __                            __  _    ")
+	print("   / / / /__  _______  ______ _____  / /_(_)   ")
+	print("  / /_/ / _ \/ ___/ / / / __ `/ __ \/ __/ /    ")
+	print(" / __  /  __/ /  / /_/ / /_/ / / / / /_/ /     ")
+	print("/_/ /_/\___/_/   \__, /\__,_/_/ /_/\__/_/      ")
+	print("                /____/                         \n")
+	print(w + "   [+] Nama  : ")
+	print("   [-] Npm   : ")
+	print("   [-] Divisi: ")
+	print("   [-] Hoby  : \n")
+	pilih()
