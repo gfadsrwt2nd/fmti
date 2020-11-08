1ca7f6983b49075010571b1e9863645f8a702cd4
gmzr0x <55633963+gmzr0x@users.noreply.github.com>
2020-11-09 01:52:11 +0800

    Add files via upload

---
+++ anggota/ketum/hendy.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+def hendy():
+	time.sleep(0.8)
+	os.system('clear')
+	print(b + "    __  __               __         _____                        __  __        ")
+	print("   / / / /__  ____  ____/ /_  __   / ___/__  ___________ _____  / /_/ /_  ____ ")
+	print("  / /_/ / _ \/ __ \/ __  / / / /   \__ \/ / / / ___/ __ '/ __ \/ __/ __ \/ __ \ ")
+	print(" / __  /  __/ / / / /_/ / /_/ /   ___/ / /_/ (__  ) /_/ / / / / /_/ / / / /_/ / ")
+	print("/_/ /_/\___/_/ /_/\__,_/\__, /   /____/\__,_/____/\__,_/_/ /_/\__/_/ /_/\____/")
+	print("                       /____/ \n")
+	print(w + "   [+] Nama  : Hendy Susantho")
+	print("   [-] Npm   : 1813015")
+	print("   [-] Divisi: Ketua Umum")
+	print("   [-] Hoby  : Olahraga\n")
+	pilih()
