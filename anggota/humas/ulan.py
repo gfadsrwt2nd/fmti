f39b2b2c455c134a263bae3beb00c0c33aedbc8b
gmzr0x <55633963+gmzr0x@users.noreply.github.com>
2020-11-09 02:02:43 +0800

    Add files via upload

---
+++ anggota/humas/ulan.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+def ulan():
+	time.sleep(0.8)
+	os.system('clear')
+	print(" _       __      __               _____             _ __       _  ")
+	print("| |     / /_  __/ /___ _____     / ___/____ _____  (_) /______(_) ")
+	print("| | /| / / / / / / __ `/ __ \    \__ \/ __ `/ __ \/ / __/ ___/ /  ")
+	print("| |/ |/ / /_/ / / /_/ / / / /   ___/ / /_/ / /_/ / / /_/ /  / /   ")
+	print("|__/|__/\__,_/_/\__,_/_/ /_/   /____/\__,_/ .___/_/\__/_/  /_/    ")
+	print("                                         /_/                    \n ")
+	print(w + "   [+] Nama  : Ulan Sapitri")
+	print("   [-] Npm   : 1913100")
+	print("   [-] Divisi: Humas")
+	print("   [-] Hoby  : \n")
+	pilih()
