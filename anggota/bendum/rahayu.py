017174ea5cd86b61f3f834107f144e7eeddf7675
gmzr0x <55633963+gmzr0x@users.noreply.github.com>
2020-11-09 01:50:30 +0800

    Add files via upload

---
+++ anggota/bendum/rahayu.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+def rahayu():
+	time.sleep(0.8)
+	os.system('clear')
+	print("  ______     _    ____        __                     ")
+	print(" /_  __/____(_)  / __ \____ _/ /_  ____ ___  ____  __ ")
+	print("  / / / ___/ /  / /_/ / __ `/ __ \/ __ `/ / / / / / / ")
+	print(" / / / /  / /  / _, _/ /_/ / / / / /_/ / /_/ / /_/ / ")
+	print("/_/ /_/  /_/  /_/ |_|\__,_/_/ /_/\__,_/\__, /\__,_/  ")
+	print("                                      /____/ \n")
+	print(w + "   [+] Nama  : Tri Rahayu")
+	print("   [-] Npm   : 1913035")
+	print("   [-] Divisi: Bendahara")
+	print("   [-] Hoby  : \n")
+	pilih()
