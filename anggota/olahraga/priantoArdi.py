39016058ae0337c7eb5cc0b87164551669c4a030
gmzr0x <55633963+gmzr0x@users.noreply.github.com>
2020-11-09 02:09:04 +0800

    Add files via upload

---
+++ anggota/olahraga/priantoArdi.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+def priantoArdi():
+	time.sleep(0.8)
+	os.system('clear')
+	print("    ___             ___    ____       _             __       ")
+	print("   /   |  _________/ (_)  / __ \_____(_)___ _____  / /_____  ")
+	print("  / /| | / ___/ __  / /  / /_/ / ___/ / __ `/ __ \/ __/ __ \ ")
+	print(" / ___ |/ /  / /_/ / /  / ____/ /  / / /_/ / / / / /_/ /_/ / ")
+	print("/_/  |_/_/   \__,_/_/  /_/   /_/  /_/\__,_/_/ /_/\__/\____/  \n")
+	print(w + "   [+] Nama  : ")
+	print("   [-] Npm   : ")
+	print("   [-] Divisi: ")
+	print("   [-] Hoby  : \n")
+	pilih()
---
+++ anggota/olahraga/vino.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+def vino():
+	time.sleep(0.8)
+	os.system('clear')
+	print("    __  ___     ___    __      _                 ")
+	print("   /  |/  /    /   |  / /   __(_)___ _____  ____  ")
+	print("  / /|_/ /    / /| | / / | / / / __ `/ __ \/ __ \ ")
+	print(" / /  / /    / ___ |/ /| |/ / / /_/ / / / / /_/ / ")
+	print("/_/  /_(_)  /_/  |_/_/ |___/_/\__,_/_/ /_/\____/  \n")
+	print(w + "   [+] Nama  : M.Alviano")
+	print("   [-] Npm   : 1913017")
+	print("   [-] Divisi: Olahraga")
+	print("   [-] Hoby  : Gaming\n")
+	pilih()
