f60175c7cffb7d1d5dd189ddd3b03cf80ca42241
gmzr0x <55633963+gmzr0x@users.noreply.github.com>
2020-11-09 02:07:20 +0800

    Add files via upload

---
+++ anggota/kominfo/rindi.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+def rindi():
+	time.sleep(0.8)
+	os.system('clear')
+	print("    ____  _           ___    _____            __  _       ")
+	print("   / __ \(_)___  ____/ (_)  / ___/___  ____  / /_(_)___ _ ")
+	print("  / /_/ / / __ \/ __  / /   \__ \/ _ \/ __ \/ __/ / __ `/ ")
+	print(" / _, _/ / / / / /_/ / /   ___/ /  __/ /_/ / /_/ / /_/ /  ")
+	print("/_/ |_/_/_/ /_/\__,_/_/   /____/\___/ .___/\__/_/\__,_/   ")
+	print("                                   /_/                   \n")
+	print(w + "   [+] Nama  : Rindi Septia")
+	print("   [-] Npm   : 1913098 ")
+	print("   [-] Divisi: Kominfo")
+	print("   [-] Hoby  : Melakukan hal yang bermanfaat\n")
+	pilih()
---
+++ anggota/kominfo/zeni.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+def zeni():
+	time.sleep(0.8)
+	os.system('clear')
+	print(b + " _____              _  ") 
+	print("/__  /  ___  ____  (_) ") 
+	print("  / /  / _ \/ __ \/ / ") 
+	print(" / /__/  __/ / / / /  ")
+	print("/____/\___/_/ /_/_/   \n")  
+	print(w + "   [+] Nama  : Zeni")
+	print("   [-] Npm   : 1913035")
+	print("   [-] Divisi: Kominfo")
+	print("   [-] Hoby  : Desain\n")
+	pilih()
