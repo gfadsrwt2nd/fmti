f628446e47583b93b4b7eed9e41de2a0dde073ef
gmzr0x <55633963+gmzr0x@users.noreply.github.com>
2020-11-09 02:00:54 +0800

    Add files via upload

---
+++ anggota/evaluasi/ristiana.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+def ristiana():
+	time.sleep(0.8)
+	os.system('clear')
+	print("    ____  _      __  _                      ")
+	print("   / __ \(_)____/ /_(_)___ _____  ____ _    ")
+	print("  / /_/ / / ___/ __/ / __ `/ __ \/ __ `/    ")
+	print(" / _, _/ (__  ) /_/ / /_/ / / / / /_/ /     ")
+	print("/_/ |_/_/____/\__/_/\__,_/_/ /_/\__,_/      \n")
+	print(w + "   [+] Nama  : ")
+	print("   [-] Npm   : ")
+	print("   [-] Divisi: ")
+	print("   [-] Hoby  : \n")
+	pilih()
---
+++ anggota/evaluasi/windi.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+def windi():
+	time.sleep(0.8)
+	os.system('clear')
+	print(" _       ___           ___             __  _  ")
+	print("| |     / (_)___  ____/ (_)_  ______ _/ /_(_) ")
+	print("| | /| / / / __ \/ __  / / / / / __ `/ __/ /  ")
+	print("| |/ |/ / / / / / /_/ / / /_/ / /_/ / /_/ /   ")
+	print("|__/|__/_/_/ /_/\__,_/_/\__, /\__,_/\__/_/    ")
+	print("                       /____/                 \n")
+	print(w + "   [+] Nama  : Windiyati ")
+	print("   [-] Npm   : 1913034")
+	print("   [-] Divisi: Evaluasi")
+	print("   [-] Hoby  : \n")
+	pilih()
