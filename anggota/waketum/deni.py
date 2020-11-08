1365553a6bec2402508b7b28e30936f3874e4098
gmzr0x <55633963+gmzr0x@users.noreply.github.com>
2020-11-09 01:53:28 +0800

    Add files via upload

---
+++ anggota/waketum/deni.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+def deni():
+	time.sleep(0.8)
+	os.system('clear')
+	print(b + "     ____             _ ")
+	print("    / __ \___  ____  (_) ")
+	print("   / / / / _ \/ __ \/ / ")
+	print("  / /_/ /  __/ / / / /  ")
+	print(" /_____/\___/_/ /_/_/   \n")             
+	print(w + "   [+] Nama  : Deni")
+	print("   [-] Npm   : 1913008")
+	print("   [-] Divisi: Wakil Ketua Umum")
+	print("   [-] Hoby  : Olahraga\n")
+	pilih()
