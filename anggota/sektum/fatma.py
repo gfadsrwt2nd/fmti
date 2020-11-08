cd36a7f5cbae2cb8945c03f59c07e49e3110ab7c
gmzr0x <55633963+gmzr0x@users.noreply.github.com>
2020-11-09 02:13:36 +0800

    Add files via upload

---
+++ anggota/sektum/fatma.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+def fatma():
+	time.sleep(0.8)
+	os.system('clear')
+	print(b + "   _____      _    ______      __                                 __  _    ")
+	print("  / ___/_____(_)  / ____/___ _/ /_____ ___  ____ __      ______ _/ /_(_)   ")
+	print("  \__ \/ ___/ /  / /_  / __ `/ __/ __ `__ \/ __ `/ | /| / / __ `/ __/ /    ")
+	print(" ___/ / /  / /  / __/ / /_/ / /_/ / / / / / /_/ /| |/ |/ / /_/ / /_/ /     ")
+	print("/____/_/  /_/  /_/    \__,_/\__/_/ /_/ /_/\__,_/ |__/|__/\__,_/\__/_/     \n") 
+	print(w + "   [+] Nama  : Sri Fatmawati")
+	print("   [-] Npm   : 1813030")
+	print("   [-] Divisi: Sekertaris Umum")
+	print("   [-] Hoby  : Masak\n")
+	pilih()
---
+++ anggota/sektum/melisa.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+def melisa():
+	time.sleep(0.8)
+	os.system('clear')
+	print(b + "     __  ___     ___               ___       __     __    _    ")
+	print("    /  |/  /__  / (_)________ _   /   | ____/ /__  / /_  (_)   ")
+	print("   / /|_/ / _ \/ / / ___/ __ `/  / /| |/ __  / _ \/ __ \/ /    ")
+	print("  / /  / /  __/ / (__  ) /_/ /  / ___ / /_/ /  __/ /_/ / /     ")
+	print(" /_/  /_/\___/_/_/____/\__,_/  /_/  |_\__,_/\___/_.___/_/    \n")  
+	print(w + "   [+] Nama  : Muhammad Rafli Gentapratama")
+	print("   [-] Npm   : 1913016")
+	print("   [-] Divisi: Sekertaris Umum")
+	print("   [-] Hoby  : Belajar\n")
+	pilih()
